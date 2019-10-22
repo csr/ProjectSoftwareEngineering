878e08d930ef24d5d7bcee0acdc8a6eb6c46176c
Cesare De Cal <hi@cesare.io>
2019-10-22 18:16:58 +0200

    Renamed module from "module" to "Project"

--- SubwayGroupA/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ SubwayGroupA/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -52,5 +52,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/module.dir/DependInfo.cmake"
+  "CMakeFiles/Project.dir/DependInfo.cmake"
   )
