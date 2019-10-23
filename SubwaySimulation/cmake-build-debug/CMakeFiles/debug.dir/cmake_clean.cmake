0425aae434342965d9c3fa82a509c9a221ddb3dd
Cesare De Cal <hi@cesare.io>
2019-10-23 18:41:31 +0200

    Major refactor (added release and debug targets, added GTest, changed project name)

---
+++ SubwaySimulation/cmake-build-debug/CMakeFiles/debug.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/debug.dir/Station.cpp.o"
+  "CMakeFiles/debug.dir/Subway.cpp.o"
+  "CMakeFiles/debug.dir/Tram.cpp.o"
+  "CMakeFiles/debug.dir/tinystr.cpp.o"
+  "CMakeFiles/debug.dir/tinyxml.cpp.o"
+  "CMakeFiles/debug.dir/tinyxmlerror.cpp.o"
+  "CMakeFiles/debug.dir/tinyxmlparser.cpp.o"
+  "debug"
+  "debug.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
