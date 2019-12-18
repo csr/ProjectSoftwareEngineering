0f4be34c1c364acd11a5da0540610465eab217e8
vorsanigo <56628551+vorsanigo@users.noreply.github.com>
2019-12-18 09:06:23 +0100

    Done contracts

--- SubwaySimulation/Track.h
+++ SubwaySimulation/Track.h
@@ -25,34 +25,118 @@ class Station;
 
 class Track {
  public:
+ 
+  /**
+     *Constructs a Track class with track number, next station, previous station as string
+     * @note ENSURE(properlyInitialized(), "constructor must end in properlyInitialized state");
+     * @param track
+     * @param next
+     * @param previous
+     * @returns an instance of Track
+     */
   Track(int track, string next, string previous);
 
+ /**
+     *Constructs a Track class with track number, next station, previous station as pointers
+     * @note ENSURE(properlyInitialized(), "constructor must end in properlyInitialized state");
+     * @param track
+     * @param next
+     * @param previous
+     * @returns an instance of Track
+     */
   Track(int track, Station* next, Station* previous);
 
   bool properlyInitialized();
 
+ /**
+   * Setter for the number of the Track
+   * @note REQUIRE(this->properlyInitialized(), "Track wasn't properly initialized");
+   * @note ENSURE(ValidIntegerAttribute(number), "Number of track is negative");
+   * @note ENSURE(number == getTrack(), "Number of track is negative");
+   * @param track
+   */
   void setTrack(int track);
 
+ /**
+   * Setter for the next Station as pointer
+   * @note REQUIRE(this->properlyInitialized(), "Track wasn't properly initialized");
+   * @note ENSURE(next == getNext(), "Next station wasn't set correctly");
+   * @param next
+   */
   void setNext(Station* next);
 
+ /**
+   * Setter for the previous Station as pointer
+   * @note REQUIRE(this->properlyInitialized(), "Track wasn't properly initialized when calling setPrevious");
+   * @note ENSURE(previous == getPrevious(), "Previous station wasn't set correctly");
+   * @param previous
+   */
   void setPrevious(Station* previous);
 
+ /**
+   * Setter for next Station as string
+   * @note REQUIRE(this->properlyInitialized(), "Track wasn't properly initialized");
+   * @note ENSURE(nextStr == getNextStr(), "Next station wasn't set correctly");
+   * @param next
+   */
   void setNextStr(string next);
 
+ /**
+   * Setter for previous Station as string
+   * @note REQUIRE(this->properlyInitialized(), "Track wasn't properly initialized when calling setPreviousStr");
+   * ENSURE(previousStr == getPreviousStr(), "Previous station wasn't set correctly");
+   * @param previous
+   */
   void setPreviousStr(string previous);
 
+ /**
+   * Getter for the number of the Track
+   * @note REQUIRE(this->properlyInitialized(), "Track wasn't properly initialized when calling getTrack");
+   * @return Number of Track
+   */
   int getTrack();
 
+ /**
+   * Getter for the next Station as pointer
+   * @note REQUIRE(this->properlyInitialized(), "Track wasn't properly initialized when calling getNextStr");
+   * @return Next Station
+   */
   Station* getNext();
 
+ /**
+   * Getter for the previous Station as pointer
+   * @note REQUIRE(this->properlyInitialized(), "Track wasn't properly initialized when calling getPrevious");
+   * @return Previous Station
+   */
   Station* getPrevious();
 
+  /**
+   * Getter for the next Station as string
+   * @note REQUIRE(this->properlyInitialized(), "Track wasn't properly initialized when calling getNextStr");
+   * @return Next Station
+   */
   string getNextStr();
 
+ /**
+   * Getter for the previous Station as string
+   * @note  REQUIRE(this->properlyInitialized(), "Track wasn't properly initialized when calling getPreviousStr");
+   * @return Previous Station
+   */
   string getPreviousStr();
 
+ /**
+   * Says if a Track is currently occupied by a particular Tram
+   * @note REQUIRE(this->properlyInitialized(), "Station wasn't initialized when calling isCurrentlyOccupied");
+   * @return If the Track is occupied
+   */
   bool isCurrentlyOccupied();
 
+ /**
+   * Setter for Track to say that it is occupied by a Tram
+   * @note REQUIRE(this->properlyInitialized(), "Station wasn't initialized when calling setOccupied");
+   * @note ENSURE(isOccupied == isCurrentlyOccupied(), "Station occupied was not set correctly");
+   * @param isOccupied
+   */
   void setOccupied(bool isOccupied);
 
 private:
